017c585c43af814b2b27fe1d8c9f6db32ff9db0b
dengjulie <juliedeng@berkeley.edu>
2018-01-11 23:09:23 -0800

    login landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
     apiKey: 'AIzaSyDjD-mG2EHBQ_4DdYFJQ2WeAEwjPeqd7bw',
@@ -14,13 +16,42 @@ class App extends Component {
     storageBucket: 'auth-56c77.appspot.com',
     messagingSenderId: '499941755717'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <Button>
+              Log Out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={{ paddingTop: 200 }} >
+            <Spinner size="large" />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
       <View>
-      <Header headerText="Authentication" />
-      <LoginForm />
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
