d9464a3267078bc25ed400d25f226d8f7c19178e
dengjulie <juliedeng@berkeley.edu>
2018-01-11 22:00:29 -0800

    added firebase and basic components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyDjD-mG2EHBQ_4DdYFJQ2WeAEwjPeqd7bw',
+    authDomain: 'auth-56c77.firebaseapp.com',
+    databaseURL: 'https://auth-56c77.firebaseio.com',
+    projectId: 'auth-56c77',
+    storageBucket: 'auth-56c77.appspot.com',
+    messagingSenderId: '499941755717'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>An app</Text>
+      <Header headerText="Authentication" />
+      <LoginForm />
       </View>
     );
   }
